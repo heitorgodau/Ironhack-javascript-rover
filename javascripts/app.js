88ebf9727e26f46baa6e9ddad31b3235f0e74a31
Heitor Godau <heitorgodau@gmail.com>
2019-03-19 17:35:39 -0300

    Mudança de direção do Rover implementado

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,13 +1,43 @@
 // Rover Object Goes Here
 // ======================
-
+let rover = {
+  direction: "N"
+}
 // ======================
 function turnLeft(rover){
   console.log("turnLeft was called!");
+  switch(rover.direction){
+    case "N":
+      rover.direction = "W"
+      return rover.direction;
+    case "W":
+      rover.direction = "S"
+      return rover.direction;
+    case "S":
+      rover.direction = "E"
+      return rover.direction;
+    case "E":
+      rover.direction = "N"
+      return rover.direction;
+  }
 }
 
 function turnRight(rover){
   console.log("turnRight was called!");
+  switch(rover.direction){
+    case "N":
+      rover.direction = "E"
+      return rover.direction;
+    case "E":
+      rover.direction = "S"
+      return rover.direction;
+    case "S":
+      rover.direction = "W"
+      return rover.direction;
+    case "W":
+      rover.direction = "N"
+      return rover.direction;
+  }
 }
 
 function moveForward(rover){
